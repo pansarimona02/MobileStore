157042a7fbeb42adc18dab1893e54c63256b75ca
pansarimona02 <35260742+pansarimona02@users.noreply.github.com>
2018-10-27 22:46:43 +0530

    Add files via upload

--- cart.php
+++ cart.php
@@ -163,35 +163,14 @@ if(!isset($_SESSION['sess_user']))
 
 
 							</table></p>
-							<button onclick="myFunction()">AddToCart</button>
-							<?php
-												if($ex==1){
-														echo "<script type='text/javascript'>	function myFunction() {alert('Please login!')	}</script>";}
-												 else {
-													 		echo "<script>
-	 											 		function myFunction() {
-	     											location.replace('https://www.w3schools.com')
-	 												}
-	 												</script>";}
-
-
-							?>
+						
 		    		</div>
 						</div>
 
 
 		    		<div class="clear"> </div>
 
-						<div class="menu_container">
-						<p class="menu_head">Lorem Ipsum<span class="plusminus">+</span></p>
-							<div class="menu_body" style="display: none;">
-							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-							</div>
-						<p class="menu_head">About Product<span class="plusminus">+</span></p>
-							<div class="menu_body" style="display: none;">
-							<p>theonlytutorials.com is providing a great varitey of tutorials and scripts to use it immediate on any project!</p>
-							</div>
-					</div>
+
 			</div>
   	</div>
 
